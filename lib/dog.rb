0d5bf0fe303f7227460bea717aa0f394341154e8
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-29 00:02:45 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+  def initialize (name, breed = 'Mutt')
+    @name= name
+    @breed= breed
+  end
+
+end
+#instance_variable_get(:@name)
+#instance_variable_get(:@breed)
+#mutt.instance_variable_set(:@breed, "Mutt")
